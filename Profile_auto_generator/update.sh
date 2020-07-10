3b2a912a8fa8047e5169e8fa3fbc1ab1e97ab17d
f0rk <dummy.github.1945@gmail.com>
2020-07-10 14:34:55 +0900

    Update Script

--- Profile_auto_generator/update.sh
+++ Profile_auto_generator/update.sh
@@ -22,3 +22,41 @@ git config --global user.email ${GITH_EMAIL}
 COMMIT_MESSAGE="Profile: Update!"
 README_TEMPLATE="${PWD}"
 README_DEPLOY=$(dirname ${PWD})
+REMOTE_API="https://api.github.com/users/${GITH_USER}/repos?sort=updated&per_page=5&page=1"
+
+
+function main(){
+  echo "Setupping ..."
+  (cd git_getter;yarn install;node main.js)
+
+  TMP_FILE=$(tempfile)
+  echo "Getting Your Repo..."
+  REPO_DATA=$(cat "./repos.json")
+  MAX_COUNTER=$(echo ${REPO_DATA} | jq '.[].name' -r | wc -l);
+
+  cat ./Header.md >> $TMP_FILE
+  cat ./list/repo_list_header.md >> $TMP_FILE
+  for (( count=0; count<${MAX_COUNTER}; count++));do
+     Repo_name=$(echo $REPO_DATA | jq .[$count].name -r)
+     Repo_url=$(echo $REPO_DATA | jq .[$count].clone_url -r)
+     update_time=`date -d $(echo $REPO_DATA | jq .[$count].updated_at -r) '+%F %R'`
+    eval "echo \"$(eval cat ${README_TEMPLATE}/list/body.md)\"" >> ${TMP_FILE}
+  done
+  cat ./Footer.md >> $TMP_FILE
+  mv ${TMP_FILE} ./README.md
+  git diff --exit-code --quiet -- ./README.md ../README.md
+  if [ $? == 0 ];then
+    echo "No Update"
+    git reset
+    return 0
+  else
+    echo "Update README.md";
+    mv ./README.md ../README.md
+    git add ../README.md;
+    git commit -m "Auto Update README.md"
+    git push
+    git reset
+  fi
+}
+
+main
